2cb20568ec05c235e01d9b96e0b159a78a72df88
RPWard <ped.rward@gmail.com>
2025-08-15 18:29:10 -0500

    eslint security added

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-export default [
+export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,  // Add this line
-      }
-    }
+    plugins: { js },
+    extends: ["js/recommended"],
   },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+]);
