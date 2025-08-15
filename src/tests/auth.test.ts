aceb0894babc24e7d888c65109f2bcdd4573759d
RPWard <ped.rward@gmail.com>
2025-08-15 14:10:20 -0500

    added tests, one intentionally broken

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from 'src/api/auth';
+import { IncomingHttpHeaders } from "http";
+
+
+describe("getAPIKey", () => {
+  test("valid API Key", () => {
+    const testHeader = { "authorization": "ApiKey 1234567890" };
+    const key = getAPIKey(testHeader);
+    expect(key).toEqual("1234567890");
+  });
+
+  test("malformed API key", () => {
+    const testHeader = { "authorization": "ApiKey1234567890" };
+    const key = getAPIKey(testHeader);
+    expect(key).toBeNull
+  });
+
+  test("missing authorization field", () => {
+    const testHeader = { "apikey": "ApiKey 1234567890" };
+    const key = getAPIKey(testHeader);
+    expect(key).toBeNull
+  });
+
+  test("broken test", () => {
+    const testHeader = { "apikey": "ApiKey 1234567890" };
+    const key = getAPIKey(testHeader);
+    expect(key).toEqual("!2345");
+  });
+});
+
