c31028d100c9a1d379a56cbe111c45cb1abcf612
RPWard <ped.rward@gmail.com>
2025-08-15 17:29:58 -0500

    added formatting check to ci

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,25 +1,23 @@
 import { describe, expect, test } from "vitest";
-import { getAPIKey } from 'src/api/auth';
+import { getAPIKey } from "src/api/auth";
 import { IncomingHttpHeaders } from "http";
-
 
 describe("getAPIKey", () => {
   test("valid API Key", () => {
-    const testHeader = { "authorization": "ApiKey 1234567890" };
+    const testHeader = { authorization: "ApiKey 1234567890" };
     const key = getAPIKey(testHeader);
     expect(key).toEqual("1234567890");
   });
 
   test("malformed API key", () => {
-    const testHeader = { "authorization": "ApiKey1234567890" };
+    const testHeader = { authorization: "ApiKey1234567890" };
     const key = getAPIKey(testHeader);
-    expect(key).toBeNull
+    expect(key).toBeNull;
   });
 
   test("missing authorization field", () => {
-    const testHeader = { "apikey": "ApiKey 1234567890" };
+    const testHeader = { apikey: "ApiKey 1234567890" };
     const key = getAPIKey(testHeader);
-    expect(key).toBeNull
+    expect(key).toBeNull;
   });
 });
-
